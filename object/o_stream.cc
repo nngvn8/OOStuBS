3ee0625d279849ead45aec90a7e91a75c4cf835d
PmupHutt <benedict.mehnert@gmail.com>
2023-04-10 17:29:26 +0200

    Add convert_long_to_list, takes long(neg or pos) and converts is into a list ( <list>) (respectively signed), returns that list

--- object/o_stream.cc
+++ object/o_stream.cc
@@ -17,5 +17,37 @@
 /*****************************************************************************/
 
 #include "object/o_stream.h"
+#include <list>
+
+
+std::list<char> convert_long_to_list(long i){
+    bool is_negative = false;
+
+    if (i < 0){
+        is_negative = true;
+        i *= (-1);
+    }
+
+    std::list<char> char_list;
+    //val to compute modula with
+    long mod_ctr = 10;
+    int char_to_append;
+
+    //calc modulo and then multiply mod_ctr and subtract!
+    while(i != 0){
+        char_to_append = i % mod_ctr;
+        i = i-char_to_append;
+        char_to_append = char_to_append / (mod_ctr/10);
+        //0..9 starts at 48 with the 0
+        char_list.push_front(char_to_append + 48);
+        mod_ctr = mod_ctr * 10;
+    }
+
+    // '-' is the 45 in the ascii table
+    if(is_negative) char_list.push_front(45);
+    //char list is reversed since we use push front
+    return char_list;
+
+}
 
 /* Add your code here */ 
