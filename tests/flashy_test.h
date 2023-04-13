9b405c03d1e5720a03737bc9351d46c2857e0c64
Julius Gilka-Bötzow <j.gilkaboetzow@gmail.com>
2023-04-13 17:21:22 +0200

    Expand flashy test to ask for input

--- tests/flashy_test.h
+++ tests/flashy_test.h
@@ -3,10 +3,33 @@
 //
 
 #include "device/cgastr.h"
+#include "machine/keyctr.h"
+#include "cga_wrapping_test.h"
 
 /// The idea of this class is to encompass all other test and make them be callable by the user
 
+
+
 void test_all_tests(){
+    clear_screen(); // from wrapping test
+
     CGA_Stream cga = CGA_Stream();
-    cga << "Hello World!" << CGA_Stream::endl;
+    Keyboard_Controller keyboard = Keyboard_Controller();
+
+    cga << "Welcome. Please select a program. We have:" << CGA_Stream::endl;
+    cga << "1. Wrapping test" << CGA_Stream::endl;
+    cga << "2. Keyboard test (currently never ending)" << CGA_Stream::endl;
+    cga << "Please input one of the numbers to select a program" << CGA_Stream::endl;
+    Key number = keyboard.key_hit();
+    switch (number.ascii()) {
+        case '1':
+            cga << "Selected 1" << CGA_Stream::endl;
+            break;
+        case '2':
+            cga << "Selected 2" << CGA_Stream::endl;
+            break;
+        default:
+            cga << "Your input was " << number.ascii() << ", which is supported" << CGA_Stream::endl;
+            break;
+    }
 }
