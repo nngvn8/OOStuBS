82702190b5dc266365d2e649dd362c166744af49
PmupHutt <benedict.mehnert@gmail.com>
2023-04-14 13:54:06 +0200

    Add o_stream_test more tests, add functionality of only printing a zero.

--- tests/o_stream_test.h
+++ tests/o_stream_test.h
@@ -4,9 +4,40 @@ void o_stream_hello_w(){
 
     CGA_Stream cga_stream = CGA_Stream();
     cga_stream << "Hello? World!" << O_Stream::endl;
-    cga_stream << 69 << O_Stream::bin;
+    cga_stream << "ccc" << O_Stream::endl;
+    cga_stream << "93248093284032" << O_Stream::endl;
+
+
+    int a = 69000;
+    int b = 0;
+    unsigned long c = 2131212323423322342;
+    short d = -1;
+    cga_stream << O_Stream::dec;
+
+    cga_stream << a << O_Stream::bin << O_Stream::endl;
+    cga_stream << a << O_Stream::oct<< O_Stream::endl;
+
+    cga_stream << a << O_Stream::dec<< O_Stream::endl;
+    cga_stream << a << O_Stream::hex<< O_Stream::endl;
+
+    cga_stream << &a << O_Stream::endl;
+
+    cga_stream << b << O_Stream::endl;
+    cga_stream << &b << O_Stream::endl;
+
+    cga_stream << c << O_Stream::endl;
+    cga_stream << &c << O_Stream::endl;
+
+    cga_stream << d << O_Stream::endl;
+
     cga_stream << "osdifjwoeirjwoeirjweiorjwoeirjwoaljlwejqwiojeqwoiejqowiejqoiwejqoiwejqoweijqwoiejeirjoi";
-    cga_stream.flush();
+
+    cga_stream << b << O_Stream::endl;
+
+    cga_stream << "qwert,zuio,asdf,ghjk,lyxc,vbna,sd";
+
+    cga_stream << '\n'<< O_Stream::endl;
+
 
 
 
