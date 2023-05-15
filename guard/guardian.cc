fac2f427611a8ea333f563589c1db0f3de7b3d5b
nngvn8 <smartin-online@web.de>
2023-05-15 13:24:18 +0200

    Refined guardian function

--- guard/guardian.cc
+++ guard/guardian.cc
@@ -19,9 +19,8 @@ extern "C" void guardian (unsigned int slot);
 /*           a later point in time.                                        */
 
 void guardian (unsigned int slot){
-    bool prologue_done = plugbox.report(slot).prologue();
-    if (prologue_done) {
-        //TODO call relay function
-        //Guard::relay(plugbox.report(slot));
+    Gate* gate_ptr = &plugbox.report(slot);
+    if (gate_ptr->prologue()) {
+        guard.relay(gate_ptr);
     }
 }
