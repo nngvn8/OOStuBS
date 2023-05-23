c43e1aacd6c77ada4d5a16f9d18c77ac53727361
Julius Gilka-Bötzow <j.gilkaboetzow@gmail.com>
2023-05-23 15:44:08 +0200

    Add function definitions of coroutine

--- thread/coroutine.h
+++ thread/coroutine.h
@@ -21,8 +21,32 @@
 
 class Coroutine {
 public:
-	Coroutine(const Coroutine &copy) = delete; // prevent copying
-/* Add your code here */ 
+    Coroutine(const Coroutine &copy) = delete; // prevent copying
+
+    /**
+     * Initializes the stack pointer to point to tos and on first activation execution begins with the kickoff function
+     * @param tos
+     */
+    Coroutine(void* tos);
+
+    /**
+     * Used for the first activation of the first coroutine in the system, therefore no register values must be saved here
+     */
+    void go();
+
+    /**
+     * Triggers a coroutine switch.
+     * Saves the current contents of the non-volatile registers into the toc element and replaces them by the
+     * values of the next coroutine
+     * @param next the next coroutine to be called
+     */
+    void resume(Coroutine& next);
+
+    /**
+     * Actually performs the job of the coroutine.
+     * To enable different actions to be implemented, things functions must be implemented in Application
+     */
+    virtual void action() = 0;
 };
 
 #endif
