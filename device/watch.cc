612b8198e1bc5d8bebf6a84961848a66e635b983
Julius Gilka-Bötzow <j.gilkaboetzow@gmail.com>
2023-06-13 15:33:41 +0200

    Implement initial version of watch

--- device/watch.cc
+++ device/watch.cc
@@ -9,9 +9,24 @@
 /* process switch if necessary.                                              */
 /*****************************************************************************/
 
-/* Add your code here */ 
-/* Add your code here */ 
- 
-/* Add your code here */ 
-/* Add your code here */ 
+#include "watch.h"
+#include "machine/plugbox.h"
+#include "machine/pic.h"
+#include "syscall/guarded_sheduler.h"
 
+// For debugging
+#include "cgastr.h"
+
+void Watch::windup(){
+    plugbox.assign(plugbox.timer, *this);
+    pic.allow(PIC::TIMER);
+}
+
+void Watch::prologue(){
+    // Do nothing
+}
+
+void Watch::epilogue(){
+    cga << "Timer interrupted!" << CGA_Stream::endl();
+    // guarded_scheduler.resume();
+}
--- device/watch.h
+++ device/watch.h
@@ -19,19 +19,28 @@
 
 class Watch : public Gate, public PIT {
 public:
-	// WATCH: Timer initialization, see PIT.
 	Watch(const Watch &copy) = delete; // prevent copying
-	Watch(int us) : PIT(us) {}
 
-	// WINDUP: "Winds up" the clock. To do this, the watch object must register
-	//         with the Plugbox plugbox and allow the interrupts of the timer
-	//         module with the help of the global pic object. 
-	void windup();
+    /**
+     * Initializes a timer, which periodically interrupts at intervals of approximately us microseconds
+     * @param us  number of microseconds
+     */
+    Watch(int us) : PIT(us) {}
 
-	// PROLOGUE: Contains the prologue of the interrupt handler.
+	/**
+	 * "Winds up" the clock, by registering with the plugbox and allow the interrupts of the timer
+	 * module with the help of the global pic object.
+	 */
+    void windup();
+
+	/**
+	 * ???
+	 */
 	bool prologue();
 
-	// EPILOGUE: This method triggers the process switch.
+    /**
+     * Triggers the process switch.
+     */
 	void epilogue();
 };
 
