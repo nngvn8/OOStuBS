f5e29ec039a1d17282dbe17e5e47ca8aba797cd2
Julius Gilka-Bötzow <julius.gilka-boetzow@star-dresden.de>
2023-06-27 16:40:01 +0200

    Change prologue buffer to use the Key object

--- user/appl.cc
+++ user/appl.cc
@@ -30,27 +30,3 @@ void Application::action_interrupt_synchro(){
         } // On destruction of Secure, the guard, will call all epilogues and set itself available
     }
 }
-
-
-void Application::test_prologue_keyboard_char_buffer(){
-    cga.clear_screen();
-
-    Kb_prologue_char_buf ch_buf = Kb_prologue_char_buf();
-
-    pic.forbid(PIC::KEYBOARD);
-    cga.setpos(0, 0);
-
-    ch_buf.produce('a');
-
-    ch_buf.produce('b');
-    ch_buf.produce('c');
-
-    cga << "Consume first char, now should be a" << CGA_Stream::endl;
-    cga << ch_buf.consume() << CGA_Stream::endl;
-    cga << "Consume second char, now should be b" << CGA_Stream::endl;
-    cga << ch_buf.consume() << CGA_Stream::endl;
-    cga << "Consume second char, now should be c" << CGA_Stream::endl;
-    cga << ch_buf.consume() << CGA_Stream::endl;
-    cga << "Consume second char, nothing more in the buffer" << CGA_Stream::endl;
-    cga << ch_buf.consume() << CGA_Stream::endl;
-}
--- utils/kb_prologue_char_buf.h
+++ utils/kb_prologue_char_buf.h
@@ -5,12 +5,14 @@
 #ifndef OOSTUBBS_MBJ_KB_PROLOGUE_CHAR_BUF_H
 #define OOSTUBBS_MBJ_KB_PROLOGUE_CHAR_BUF_H
 
+#include "machine/key.h"
+
 #define BUFFER_SIZE 8
 
 class Kb_prologue_char_buf{
 private:
     int size = BUFFER_SIZE;
-    char buffer[BUFFER_SIZE];
+    Key buffer[BUFFER_SIZE];
     int next_in;
     int next_out;
 
@@ -20,15 +22,16 @@ public:
         next_out = 0;
     }
 
-    void produce(char c) {
+    void produce(Key key) {
         if((next_in+1) % size == next_out) return;
-        buffer[next_in] = c;
+        buffer[next_in] = key;
         next_in = (next_in+1)%size;
     }
 
-    char consume() {
-        if(next_out == next_in) return 0;
-        char result = buffer[next_out];
+    Key consume() {
+        Key invalid;
+        if(next_out == next_in) return invalid;
+        Key result = buffer[next_out];
         next_out = (next_out +1) % size;
         return result;
     }
